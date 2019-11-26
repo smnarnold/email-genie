81fe728f7898a99c24b92f0e6ed22079f0bc1daf
Simon Arnold <arnolali@gmail.com>
2019-11-26 16:17:54 -0500

    - Rollback export.

--- email-genie.js
+++ email-genie.js
@@ -1,3 +1,5 @@
+'use strict'
+
 class EmailGenie {
   constructor(el, settings) {
     this.dom = {
@@ -71,4 +73,16 @@ class EmailGenie {
   }
 }
 
-export default EmailGenie;
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports.EmailGenie
+} else {
+  if (typeof define === 'function' && define.amd) {
+    define([], () => {
+      return EmailGenie;
+    });
+  } else {
+    window.EmailGenie = EmailGenie;
+  }
+}
+
+
